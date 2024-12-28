f3eceba71a87714c3f9777e9376d92ca16a01603
Investgrape <alex@investgrape.com>
2024-12-27 20:12:31 -0800

    Add utility functions and custom hooks

---
+++ src/app/components/ComplianceSystem/utils/format.js
@@ -0,0 +1,22 @@
+export const formatCurrency = (amount) => {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  }).format(amount);
+};
+
+export const formatDate = (dateString) => {
+  return new Date(dateString).toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric'
+  });
+};
+
+export const formatPercent = (value) => {
+  return new Intl.NumberFormat('en-US', {
+    style: 'percent',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+  }).format(value / 100);
+};
