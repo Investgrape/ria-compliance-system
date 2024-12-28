a1287320e9ab1cf034ae9911529960a9f4f5865a
Investgrape <alex@investgrape.com>
2024-12-27 19:52:37 -0800

    Simplify Next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+  },
   basePath: '/ria-compliance-system',
-  assetPrefix: '/ria-compliance-system/',
-  trailingSlash: true,
 }
 
 module.exports = nextConfig
